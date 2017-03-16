f614807d84c157e88d7763cafa0c19f0648ea102
randyhperez <randyhperez@gmail.com>
2017-03-16 11:01:24 -0400

    created and finished Have the time and I do declare js projects

---
+++ WebFundamentals/Javascript/Have_the_time.js
@@ -0,0 +1,134 @@
+var hour = ;
+// set hour to number 1-12
+var minute = ;
+// set minute 0-60
+var period = "";
+// set period to "AM" or "PM"
+
+if (minute > 60 || minute < 0 || hour < 1 || hour > 12) {
+  console.log(hour + ":" + minute + " " + period + " can't be shown on a clock dingus!");
+}
+else if (minute == 0) {
+  if (period == "AM" && hour >= 4) {
+    console.log("It's " + hour + " o'clock in the morning");
+  }
+  if (period == "PM" && hour == 12) {
+    console.log("It's noon");
+  }
+  else if (period == "PM" && hour >= 1 && hour <= 4 ) {
+    console.log("It's " + hour + " o'clock in the afternoon");
+  }
+  else if (period == "PM" && hour >= 5 && hour <= 7) {
+    console.log("It's " + hour + " o'clock in the evening");
+  }
+  else if (period == "PM" && hour >= 8 || period == "AM" && hour <= 3) {
+    console.log("It's " + hour + " o'clock at night");
+  }
+}
+else if (minute == 5) {
+  if (period == "AM" && hour >= 4) {
+    console.log("It's a quarter after " + hour + " in the morning");
+  }
+  if (period == "PM" && hour == 12) {
+    console.log("It's five after noon");
+  }
+  else if (period == "PM" && hour >= 1 && hour <= 4 ) {
+    console.log("It's " + hour + ":" + minute + " in the afternoon");
+  }
+  else if (period == "PM" && hour >= 5 && hour <= 7) {
+    console.log("It's five after " + hour + " in the evening");
+  }
+  else if (period == "PM" && hour >= 8 || period == "AM" && hour <= 3) {
+    console.log("It's five after " + hour + " at night");
+  }
+}
+else if (minute == 15) {
+  if (period == "AM" && hour >= 4) {
+    console.log("It's a quarter after " + hour + " in the morning");
+  }
+  if (period == "PM" && hour == 12) {
+    console.log("It's a quarter after noon");
+  }
+  else if (period == "PM" && hour >= 1 && hour <= 4 ) {
+    console.log("It's a quarter after " + hour +" in the afternoon");
+  }
+  else if (period == "PM" && hour >= 5 && hour <= 7) {
+    console.log("It's a quarter after " + hour + " in the evening");
+  }
+  else if (period == "PM" && hour >= 8 || period == "AM" && hour <= 3) {
+    console.log("It's a quarter after " + hour + " at night");
+  }
+}
+else if (minute == 30) {
+  if (period == "AM" && hour >= 4) {
+    console.log("It's a  half past " + hour + " in the morning");
+  }
+  if (period == "PM" && hour == 12) {
+    console.log("It's " + minute + " minutes after noon");
+  }
+  else if (period == "PM" && hour >= 1 && hour <= 4 ) {
+    console.log("It's half past " + hour + " in the afternoon");
+  }
+  else if (period == "PM" && hour >= 5 && hour <= 7) {
+    console.log("It's half past " + hour + " in the evening");
+  }
+  else if (period == "PM" && hour >= 8 || period == "AM" && hour <=3) {
+    console.log("It's half past " + hour + " at night");
+  }
+}
+else if (minute == 50) {
+  if (period == "AM" && hour <= 10 && hour >=4) {
+    hour++
+    console.log("It's a 10 til " + hour + " in the morning");
+  }
+  if (period == "AM" && hour == 11) {
+    console.log("It's 10 til noon");
+  }
+  else if (period == "PM" && hour >= 1 && hour <= 4 ) {
+    hour++
+    console.log("It's a 10 til " + hour + " in the afternoon");
+  }
+  else if (period == "PM" && hour >= 5 && hour <= 7) {
+    hour++
+    console.log("It's a 10 til " + hour + " in the evening");
+  }
+  else if (period == "PM" && hour >= 8 || period == "AM" && hour  <=3) {
+    hour++
+    console.log("It's a 10 til " + hour + " at night");
+  }
+}
+else if (minute < 30) {
+  if (period == "AM" && hour >= 4) {
+    console.log("It's just after " + hour + " in the morning.");
+  }
+  else if (period == "PM" && hour >= 12 && hour <= 4) {
+    console.log("It's just after " + hour + " in the afternoon.");
+  }
+  else if (period =="PM" && hour >= 5 && hour <=7) {
+    console.log("It's just after " + hour + " in the evening.");
+  }
+  else if (period == "PM" && hour >=8 || period == "AM") {
+    console.log("It's just after " + hour + " at night.");
+  }
+}
+ else if (minute > 30) {
+  if (period == "AM" && hour == 11) {
+    console.log("It's almost noon.");
+  }
+  else if (period == "AM" && hour >= 3) {
+    hour++
+    console.log("It's almost " + hour + " in the morning.");
+  }
+  else if (period == "PM" && hour >= 1 && hour <= 4) {
+    hour++
+    console.log("It's almost " + hour + " in the afternoon.");
+  }
+  else if (period =="PM" && hour >= 5 && hour <=7) {
+    hour++
+    console.log("It's almost " + hour + " in the evening.");
+  }
+  else if (period == "PM" && hour >=8 || period == "AM" && hour < 3) {
+    hour++
+    console.log("It's almost " + hour + " at night.");
+  }
+}
---
+++ WebFundamentals/Javascript/i_do_declare.js
@@ -0,0 +1,23 @@
+var num1 = 21;
+var num2 = -21;
+var num3 = 5.595;
+var num4 = 7*-2+10;
+
+console.log("Number 1 is " + num1 + ".");
+console.log("Number 2 is " + num2 + ".");
+console.log("Number 3 is" + num3 + ".");
+console.log("Number 4 is " + num4 + ".");
+
+var string1 = "A Suh dude";
+var string2 = "My name is Randy";
+var string3 = "I'm tired" + "always";
+
+console.log("String 1 is " + string1 + ".");
+console.log("String 2 is " + string2 + ".");
+console.log("String 3 is " + string3 + ".");
+
+var ImTired = true;
+var ImNotTired = false;
+
+console.log("I am tired? " + ImTired + "!!!!");
+console.log("I'm not tired! " + ImNotTired + "!!! Don't lie to yourself");
