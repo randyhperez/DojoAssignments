4e3e3399734c06d7dcd5a4b845311c0b9f20eb1b
randyhperez <randyhperez@gmail.com>
2017-03-17 10:20:22 -0400

    created and completed random_chance.js project

---
+++ WebFundamentals/Javascript/random_chance.js
@@ -0,0 +1,18 @@
+function slot(quarters){
+  for (var i = quarters; i >= 1; i--) {
+    var chance = Math.ceil(Math.random() * 100)
+    if (i - 1 == 0) {
+      console.log("You are out of quarters duder");
+    }
+    else if (chance <= 1) {
+      var x = Math.ceil(Math.random() * 50) +51;
+      i = i + x;
+      console.log("You won " + x + " quarters!!!");
+    }
+    else if (i >=1 ){
+      console.log("You didn't win :/ and have " + (i - 1) + " quarters left");
+    }
+  }
+}
+// enter amount to run slot machine.
+slot()
